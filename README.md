3019fbfe07930b2d12748867e39880616ac28f19
mpetrole <mattpetrole@gmail.com>
2017-02-14 18:44:57 -0500

    Fixed up to handle URIs and blank lines, generally more robust

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 IPCheck
 
-Gets and sorts IPs for given domains. Put the domains in the input.txt, and
-run ipcheck.rb, then receive output in output.txt.
+Gets and sorts IPs for given domains. Put the domains/URIs in the input.txt, and
+run ipcheck.rb, then receive output in output.txt. It will strip out the domains from URIs,
+so it is not necessary to put only domains. It will also ignore blank lines.
+
+Requires at least Ruby 2.x
--- ipcheck.rb
+++ ipcheck.rb
@@ -6,24 +6,32 @@ class Ipcheck
   end
   
   def self.check()
-    urls = Hash.new()
-    File.foreach("input.txt") do |url|
-      url = url.sub(/\Ahttps?:\/\//,"")
-      begin
-      ip = Resolv.new.getaddress(url.chomp())
-      rescue Resolv::ResolvError
-        ip = "Error, unable to resolve hostname."
+    puts "Fetching IPs..."
+    urls = Hash.new() #hash to store the url-ip pairs
+    File.foreach("input.txt") do |url| #go through the input file and get each line
+      unless url.to_s.empty? #ignore blank lines
+        domain = url.sub(/\Ahttps?:\/\//,"") #strip the scheme and any trailing bits
+        domain = domain.sub(/(?=\/).+/,"")
+        begin
+        ip = Resolv.new.getaddress(url.chomp()) #try to get the ip
+        rescue Resolv::ResolvError #don't break on error
+          ip = "Error, unable to resolve hostname."
+        end
+        urls[domain] = ip #set the domain and ip pair
       end
-      urls[url] = ip
     end
-    sorted_ip = urls.sort_by{ |url, ip|
-  ip[1].split('.').map{ |digits| digits.to_i }}
-  sorted_ip.each { |url, ip|
-  File.open("output.txt", 'a') do |result|
-      result.puts "#{url} #{ip}"
+    puts "Sorting Results..."
+    sorted_ip = urls.sort_by{ |domain, ip|
+  ip[1].split('.').map{ |digits| digits.to_i }} #this breaks the ip up into individual numbers because it's easier to ensure a good sort this way.
+  sorted_ip.each { |domain, ip|
+  File.open("output.txt", 'a') do |result| #put the sorted domain-ip pairs into the output file
+      result.puts "#{domain} #{ip}"
       end
     }
+    puts "Finished, press enter to exit" #just because I like closure
+    wait = gets
   end
+
 end
 
 Ipcheck.check
