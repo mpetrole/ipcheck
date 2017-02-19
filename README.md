a0413410e1ec05f17b27cf19f1ab602dee2e901e
mattpetrole@gmail.com <mattpetrole@gmail.com>
2017-02-19 13:12:35 -0500

    Now actually shows if the URLs might be IP phish

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ IPCheck
 
 Gets and sorts IPs for given domains. Put the domains/URIs in the input.txt, and
 run ipcheck.rb, then receive output in output.txt. It will strip out the domains from URIs,
-so it is not necessary to put only domains. It will also ignore blank lines.
+so it is not necessary to put only domains. It will also ignore blank lines and extra text AFTER the url.
 
 Requires at least Ruby 2.x
 
--- ipcheck.rb
+++ ipcheck.rb
@@ -1,40 +1,66 @@
 require 'resolv'
+require 'net/http'
+require 'uri'
 
 class Ipcheck
   
   def initialize()
   end
   
+def self.url_exist?(url_string)
+  begin
+    url = URI.parse(URI.encode(url_string))
+    req = Net::HTTP.new(url.host, url.port)
+    req.use_ssl = (url.scheme == 'https')
+    req.verify_mode = OpenSSL::SSL::VERIFY_NONE #I know this is normally bad, but we just want to connect, we don't care about the security of the connection. Please don't hate me!
+    path = url.path if ! url.path.empty? #use a path if available
+    res = req.request_head(path || '/')
+    res.code != "404" # false if returns 404 - not found
+  rescue Errno::ENOENT
+    false # false if can't find the server
+  rescue OpenSSL::SSL::SSLError
+    false #ssl can't connect
+  end
+end
+  
   def self.check()
     puts "Fetching IPs..."
     urls = Hash.new() #hash to store the url-ip pairs
-    File.foreach("input.txt") do |url| #go through the input file and get each line
+    ipPhish = Hash.new() #will use this later to store the potential IP phish
+    File.foreach("input.txt") do |url| #go through the input file and get each line, then do some changes to make the URL readable
+      url.gsub!(/\s.+/, '')
+      url.chomp!
       unless url.to_s.empty? #ignore blank lines
-        domain = url.sub(/\Ahttps?:\/\//,"") #strip the scheme and any trailing bits
-        domain = domain.sub(/(?=\/).+/,"")
+        domainNoScheme = url.sub(/\Ahttps?:\/\//,"") #strip the scheme and any trailing bits
+        domain = domainNoScheme.sub(/(?=\/).+/,"")
         unless urls.has_key? "#{domain}" #ignore duplicate domains
          begin
-          ip = Resolv.new.getaddress(domain.chomp()) #try to get the ip
-          rescue Resolv::ResolvError #don't break on error
+          ip = Resolv.new.getaddress(domain) #try to get the ip
+          rescue Resolv::ResolvError #unable to get ip
             ip = "Error, unable to resolve hostname."
           end
           urls[domain] = ip #set the domain and ip pair
+          #check to see if the urls will work with the ip subbed for the url
+          if ip != "Error, unable to resolve hostname." #ignore ones that are offline or otherwise invalid
+            new_url = url.sub("#{domain}","#{ip}")
+            ip_phish = url_exist?("#{new_url}")
+            ipPhish[url] = ip_phish
+          else 
+            ipPhish[url] = "Error, unable to resolve hostname."
+          end
         end
       end
     end
     puts "Sorting Results..."
-    sorted_ip = urls.sort_by{ |domain, ip|
-  ip[1].split('.').map{ |digits| digits.to_i }} #this breaks the ip up into individual numbers because it's easier to ensure a good sort this way.
-  sorted_ip.each { |domain, ip|
-  File.open("output.txt", 'a') do |result| #put the sorted domain-ip pairs into the output file
-      result.puts "#{domain} #{ip}"
+  ipPhish.each { |uri, ipphish| #sorted_ip
+  File.open("output.txt", 'a') do |result| #put the sorted url-possible ip phish pairs into the output file
+      result.puts "#{uri} Potential IP phish? #{ipphish}"
       end
     }
-    puts "Finished, press enter to exit" #just because I like closure
-    wait = gets
-  end
-
 end
 
-File.open('/tmp/file', 'w') {}
+File.open('output.txt', 'w') {}
 Ipcheck.check
+puts "Finished, press enter to exit" #just because I like closure
+    wait = gets
+    end
